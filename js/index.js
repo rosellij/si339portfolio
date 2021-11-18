9b54e6b73eacd847a1a0b58c81399ba27d0aef6b
rosellij <johnroselli3@gmail.com>
2021-11-17 23:46:01 -0500

    made the footer work!

--- js/index.js
+++ js/index.js
@@ -4,4 +4,8 @@ window.addEventListener('DOMContentLoaded', () => {
         document.querySelector(".flip-card").focus()
     });
 
+    document.querySelector(".footerformdiv a").addEventListener("click", function() {
+        document.querySelector(".footerformdiv").focus();
+    });
+
 });
