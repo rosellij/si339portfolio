c73e0c1f4aefcd4ee8b7570d28fe19be93f711a3
rosellij <johnroselli3@gmail.com>
2021-11-17 22:38:43 -0500

    implemented on-click flip cards

---
+++ js/index.js
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    document.querySelector(".flip-card").addEventListener("click", function() {
+        document.querySelector(".flip-card").focus()
+    });
+
+});
