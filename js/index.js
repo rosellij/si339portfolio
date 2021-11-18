f101411091b320de6df899dd1e29ef54c5e21322
rosellij <johnroselli3@gmail.com>
2021-11-18 12:47:13 -0500

    fixed footer stuff!

--- js/index.js
+++ js/index.js
@@ -1,11 +1,31 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-    document.querySelector(".flip-card").addEventListener("click", function() {
-        document.querySelector(".flip-card").focus()
-    });
+    console.log(document.querySelector("main, header"));
 
     document.querySelector(".footerformdiv a").addEventListener("click", function() {
-        document.querySelector(".footerformdiv").focus();
+        document.querySelector(".footerformdiv").classList.add("footerclicked");
     });
+
+    document.querySelectorAll("main, header").forEach(item => {
+        item.addEventListener("click", function() {
+            document.querySelector(".footerformdiv").classList.remove("footerclicked");
+        });
+    });
+
+    document.querySelector(".footerformdiv").addEventListener("focus", function() {
+        document.querySelector(".footerformdiv").classList.add("footerclicked");
+    });
+
+    document.querySelectorAll(".skip, header *, main *").forEach(item => {
+        item.addEventListener("focus", function() {
+            document.querySelector(".footerformdiv").classList.remove("footerclicked");
+        });
+    });
+
+    if (document.querySelector("main").classList.contains("page3grid")) {
+        document.querySelector(".flip-card").addEventListener("click", function() {
+            document.querySelector(".flip-card").focus();
+        });
+    };
 
 });
